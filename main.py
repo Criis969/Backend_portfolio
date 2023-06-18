34ce478af51d2bff85963cd631a7ee281cbe9a5f
Cristian Sanz <cristian.sanz.requeijo@gmail.com>
2023-06-18 18:49:31 +0200

    Se ha añadido el modelo de datos Project

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Project(BaseModel):
+    id: int
+    name: str
+    owner: str
+    link: str
+    description: str
 
 app = FastAPI()
 
@@ -9,8 +18,12 @@ async def root():
 
 @app.get("/projects")
 async def get_projects():
-    return {"message": "Return projects list"}
+    list_projects=[Project(id=0,name="Test project 1",owner="Cristian",link="http://project1.com",description="It is the test project 1"),
+                   Project(id=1,name="Test project 2",owner="Cristian",link="http://project2.com",description="It is the test project 2"),
+                   Project(id=2,name="Test project 3",owner="Cristian",link="http://project3.com",description="It is the test project 3")]
+    return list_projects
 
 @app.get("/projects/{id_project}")
 async def get_project():
-    return {"message": "Return a project"}
+    project = Project(id=0,name="Test project",owner="Cristian",link="http://project.com",description="It is a test project")
+    return project
