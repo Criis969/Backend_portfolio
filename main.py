2a2df6bd1bc4780154116451d0abb2cec9917cbd
Cristian Sanz <cristian.sanz.requeijo@gmail.com>
2023-06-20 02:20:29 +0200

    Comenzando a implementar JWT

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
 from pydantic import BaseModel
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from jose import jwt, JWTError
 
+SECRET_KEY = "688904a521337fdf3c68c477c9a497068d6eeac2fb03b411a197a918aa2163b2"
+ALGORITHM = "HS256"
 
 class Project(BaseModel):
     id: int
@@ -8,6 +14,32 @@ class Project(BaseModel):
     owner: str
     link: str
     description: str
+
+class User(BaseModel):
+    username: str
+
+class UserDB(User):
+    password: str
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl= "/login")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+#password @prueba1
+user_db = UserDB(username="admin",password="$2a$12$8nzRR5U.raL.uxMZ.iwxEOHn0RNnRpRt6tOQRyQYRUTr/qXhK1TWa")
+
+async def authenticate_user(user: UserDB):
+    if user.username != user_db.username:
+        return False
+    if not pwd_context.verify(user.password, user_db.password):
+        return False
+    expiration = datetime.utcnow() + timedelta(minutes=2)
+    token = jwt.encode(
+        {"sub": user.username, "exp": expiration},
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+    return {"access_token": token, "token_type": "bearer"}
+        
 
 app = FastAPI()
 
@@ -27,3 +59,16 @@ async def get_projects():
 async def get_project():
     project = Project(id=0,name="Test project",owner="Cristian",link="http://project.com",description="It is a test project")
     return project
+
+@app.post("/login")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    username = form_data.username
+    password = form_data.password
+    user = await authenticate_user(UserDB(username=username,password=password))
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid username or password",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    return user
